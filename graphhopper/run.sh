971aaa2560d63319a5f342674232a908a41b07ba
Peter <graphhopper@gmx.de>
2025-05-10 14:44:13 +0200

    graphhopper: enable CH

--- graphhopper/run.sh
+++ graphhopper/run.sh
@@ -10,6 +10,6 @@ if [ ! -d /data/graph-cache ]; then
 fi
 
 echo 'starting' > /data/graphhopper.status.txt
-java -Xmx4g -Xms4g \
+java -Xmx2g \
   -Ddw.graphhopper.graph.location=/data/graph-cache \
   -jar /app/graphhopper/graphhopper-web.jar server /app/graphhopper/config.yml
