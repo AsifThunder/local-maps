20d310b0309d85b11966a6e8dd08490a791359bb
Peter <graphhopper@gmx.de>
2025-05-10 13:21:24 +0200

    tileserver not yet working

--- planetiler/run.sh
+++ planetiler/run.sh
@@ -3,6 +3,10 @@ set -e
 
 mkdir -p /data
 
+# currently we only expect 'good' errors like IllegalArgumentException: file:///data/tiles.mbtiles already exists
+trap 'echo "finished" > /data/planetiler.status.txt' ERR
+trap 'echo "finished" > /data/planetiler.status.txt' EXIT
+
 if [ ! -f /data/input.osm.pbf ]; then
   echo "Downloading PBF from $PBF_URL"
   echo 'download_pbf' > /data/planetiler.status.txt
@@ -15,5 +19,3 @@ echo 'starting' > /data/planetiler.status.txt
 
 # Run planetiler if --force is specified it will overwrite the data
 java -Xmx4g -jar /app/planetiler/planetiler.jar --download --osm_path=/data/input.osm.pbf --output=/data/tiles.mbtiles
-
-echo 'finished' > /data/planetiler.status.txt
